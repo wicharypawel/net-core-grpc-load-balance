508791fa7e26d11e6ea0d8583406b80c2962abc5
Paweł Wichary <wicharypawel@gmail.com>
2020-03-11 19:17:31 +0100

    Basic external loadbalance

---
+++ NetCoreGrpc.LoadBalanceClient.ConsoleClientApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Grpc.Core;
+
+namespace NetCoreGrpc.LoadBalanceClient.ConsoleClientApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("This is just a code snippet, it is not a fully working example!!!");
+            var channelOptions = new List<ChannelOption>();
+            channelOptions.Add(new ChannelOption("grpc.lb_policy_name", "round_robin"));
+            var channel = new Channel("http://test.app.com:5000", ChannelCredentials.Insecure, channelOptions);
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NetCoreGrpc.LoadBalanceExternal.GrpcLoadBalancer/Services/KubernetesEndpointWatcher.cs
@@ -0,0 +1,61 @@
+﻿using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetCoreGrpc.LoadBalanceExternal.GrpcLoadBalancer.Services
+{
+    public sealed class KubernetesEndpointWatcher
+    {
+        private readonly Kubernetes _client;
+        private IEnumerable<EndpointEntry> _endpointEntries;
+        public KubernetesEndpointWatcher()
+        {
+            _endpointEntries = Enumerable.Empty<EndpointEntry>();
+            var config = KubernetesClientConfiguration.InClusterConfig();
+            _client = new Kubernetes(config);
+            _client.WatchNamespacedEndpointsAsync("greeter-server", "default", onEvent: onEvent);
+        }
+
+        public IReadOnlyList<EndpointEntry> GetEndpointEntries()
+        {
+            return new List<EndpointEntry>(_endpointEntries).AsReadOnly();
+        }
+
+        private void onEvent(WatchEventType type, V1Endpoints endpoint)
+        {
+            switch (type)
+            {
+                case WatchEventType.Added:
+                case WatchEventType.Modified:
+                    ModifiedEndpoints(endpoint); break;
+                case WatchEventType.Deleted:
+                case WatchEventType.Error:
+                    _endpointEntries = new List<EndpointEntry>(); break;
+                default: throw new InvalidOperationException();
+            }
+            
+        }
+
+        private void ModifiedEndpoints(V1Endpoints endpoint)
+        {
+            var list = new List<EndpointEntry>();
+            foreach (var subset in endpoint.Subsets)
+            {
+                var port = subset.Ports.First().Port;
+                foreach (var address in subset.Addresses)
+                {
+                    list.Add(new EndpointEntry() { Ip = address.Ip, Port = port });
+                }
+            }
+            _endpointEntries = list;
+        }
+    }
+
+    public sealed class EndpointEntry
+    {
+        public string Ip { get; set; }
+        public int Port { get; set; }
+    }
+}
